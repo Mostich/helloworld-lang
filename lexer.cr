b424b6272d0bef67dcfde391d3311b20150a9479
Mostich <kirill.mostimenko@gmail.com>
2020-07-22 14:41:33 +0300

    new(var): variable in lang!

--- lexer.cr
+++ lexer.cr
@@ -18,15 +18,35 @@ module Lexer
 
     def initialize(strings)
       @keyword = ""
-
+      variable = {"" => ""}
       strings.each do |x|
-        if !x.match(/(^print)\:?\s?\(?\s*"(.+)"\s*\)?/).nil?
-          keystring = x.match(/(^print)\:?\s?\(?\s*"(.+)"\s*\)?/).not_nil!
+        if !x.match(/(^print)\:?\s?\(?\s*\"(.+?)\"\s*\)?/).nil?
+          keystring = x.match(/(^print)\:?\s?\(?\s*\"(.+?)\"\s*\)?/).not_nil!
+
           quotes = keystring[2]
           @keyword = keystring[1]
-          str = "Hello\n"
           print("#{quotes.gsub("\\n", '\n')}")
         end
+
+        if !x.match(/^print[ ]*\:?\(?[ ]*(\w+)[ ]*\)?/).nil?
+          keystring = x.match(/(^print*)\:?\(?[ ]*(\w+)[ ]*\)?/).not_nil!
+          if variable.has_key?(keystring[2])
+            quotes = variable[keystring[2]]
+          else
+            quotes = keystring[2]
+          end
+
+          @keyword = keystring[1]
+          print("#{quotes.gsub("\\n", '\n')}")
+        end
+
+
+        if !x.match(/(^var)\s+(.+?)\s*\=\s*(.+)/).nil?
+          keystring = x.match(/(^var)\s+(.+?)\s*\=\s*"(.+)"/).not_nil!
+          variable[keystring[2]] = keystring[3]
+          #puts variable[keystring[2]]
+        end
+
       end
 
     end
